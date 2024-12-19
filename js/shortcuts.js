3b680fbdc4767413e99e6d2d5e791dbb3f2de5f9
jul-nat-way <92559901+jul-nat-way@users.noreply.github.com>
2024-12-19 03:30:08 +0000

    this was a triumph

--- js/shortcuts.js
+++ js/shortcuts.js
@@ -5,8 +5,7 @@ export default [
     items: {
       Github: "https://github.com",
       AWS: "https://aws.amazon.com",
-      "Dev.to": "https://dev.to",
-      Firebase: "https://console.firebase.google.com/u/0",
+      
     },
   },
   {
@@ -14,29 +13,22 @@ export default [
     color: "red",
     items: {
       Outlook: "https://outlook.office365.com/mail/inbox",
-      Canvas: "https://sit.instructure.com",
-      MyStevens: "https://login.stevens.edu/app/UserHome",
+      Canvas: "https://sequoia.instructure.com",
     },
   },
   {
-    category: "Work",
+    category: "Shit I'll never use",
     color: "green",
     items: {
-      Linkedin: "https://linkedin.com",
       Twitter: "https://twitter.com",
       Leetcode: "https://leetcode.com",
-      Handshake: "https://app.joinhandshake.com/stu/postings",
-      Indeed: "https://www.indeed.com/",
     },
   },
   {
     category: "Misc",
     color: "purple",
     items: {
-      Trakt: "https://trakt.tv/users/peterrauscher/lists",
       YouTube: "https://www.youtube.com/",
-      Fmovies: "https://fmovies.to/home",
-      Zoro: "https://zoro.to",
       TempMail: "https://tempmail.email/",
       // IPTorrents: "https://iptorrents.com/t",
     },
